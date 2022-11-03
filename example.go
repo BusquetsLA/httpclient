dd8139104fed5b79066b4c635ac277af61bcfb35
BusquetsLA <busquetsla@gmail.com>
2022-11-03 13:06:50 -0300

    example.go updated with header customization

--- example.go
+++ example.go
@@ -2,21 +2,39 @@ package main
 
 import (
 	"fmt"
+	"io/ioutil"
 	"net/http"
 
 	"github.com/BusquetsLA/httpclient/httpgo"
 )
 
+var (
+	// githubClient = httpgo.New()
+	githubClient = getGithubClient()
+)
+
 func main() {
-	client := httpgo.New()
+	getGithubUrls()
+}
 
-	headers := make(http.Header)
-	headers.Set("Authorization", "Bearer BusquetsLA")
-
-	reponse, err := client.Get("https://api.github.com", headers)
+func getGithubUrls() {
+	response, err := githubClient.Get("https://api.github.com", nil)
 	if err != nil {
 		panic(err)
 	}
 
-	fmt.Printf("response status code: %v", reponse.StatusCode)
+	bytes, err := ioutil.ReadAll(response.Body)
+	if err != nil {
+		panic(err)
+	}
+
+	fmt.Printf("status code recieved: %v, response body: %v", response.StatusCode, string(bytes))
+}
+
+func getGithubClient() httpgo.HttpClient { // just to set the headers
+	client := httpgo.New()
+	standardHeaders := make(http.Header)
+	standardHeaders.Set("Accept", "application/json")
+	client.SetHeaders(standardHeaders)
+	return client
 }
