e08aacb862189f5c8c61398f11c3df859c4b5bed
BusquetsLA <busquetsla@gmail.com>
2022-11-07 14:09:38 -0300

    adding getRequestBody UT

--- httpgo/client_core_test.go
+++ httpgo/client_core_test.go
@@ -13,13 +13,14 @@ func TestGetRequestHeaders(t *testing.T) { // rule of thumb for % of coverage: 1
 	requestHeaders.Set("User-Agent", "BusquetsLA")         // 2
 	client.Headers = requestHeaders
 
-	// Execution
+	// Execution:
 	headers := make(http.Header)
 	headers.Set("Accept", "*/*")                      // 3
 	finalHeaders := client.getRequestHeaders(headers) // the final list with every added header
 
-	// Validation
-	if len(finalHeaders) != 3 {
+	// Validation:
+	want := 3 // amount of headers set in the test
+	if got := len(finalHeaders); got != want {
 		t.Error("expected 3 headers") // this can still be HIGHLY improved
 	}
 	if finalHeaders.Get("Content-Type") != "application/json" {
@@ -31,4 +32,58 @@ func TestGetRequestHeaders(t *testing.T) { // rule of thumb for % of coverage: 1
 	if finalHeaders.Get("Accept") != "*/*" {
 		t.Error("invalid accept recieved")
 	}
+}
+
+func TestGetRequestBody(t *testing.T) {
+	// Initialization:
+	client := httpClient{}
+
+	t.Run("nil Body response: ", func(t *testing.T) {
+		// Execution:
+		body, err := client.getRequestBody("", nil)
+		// Validation:
+		if err != nil {
+			t.Error("expected no error when passing nil body")
+		}
+		if body != nil {
+			t.Error("expected nil body when passing nil body")
+		}
+	})
+	t.Run("JSON Body response: ", func(t *testing.T) {
+		requestBody := []string{"pauli", "brujita"}
+		body, err := client.getRequestBody("application/json", requestBody)
+
+		expected := (string(body))
+
+		if err != nil {
+			t.Error("expected no error when marshaling slice as JSON")
+		}
+		if string(body) != `["pauli","brujita"]` {
+			t.Errorf("expected %v, invalid JSON body obtained", expected)
+		}
+	})
+	t.Run("XML Body response: ", func(t *testing.T) {
+		requestBody := []string{"pauli", "brujita"}
+		body, err := client.getRequestBody("application/xml", requestBody)
+		expected := (string(body))
+
+		if err != nil {
+			t.Error("expected no error when marshaling slice as JSON")
+		}
+		if string(body) != `<string>pauli</string><string>brujita</string>` {
+			t.Errorf("expected %v, invalid XML body obtained", expected)
+		}
+	})
+	t.Run("default Body response: ", func(t *testing.T) {
+		requestBody := []string{"pauli", "brujita"}
+		body, err := client.getRequestBody("content/type", requestBody)
+		expected := (string(body))
+
+		if err != nil {
+			t.Error("expected no error when marshaling slice as JSON")
+		}
+		if string(body) != `["pauli","brujita"]` {
+			t.Errorf("expected %v, invalid JSON body obtained", expected)
+		}
+	})
 }
