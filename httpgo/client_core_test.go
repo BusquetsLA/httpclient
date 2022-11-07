d2b4423e72ecc84fe63e9c930c71d9e1f9a631c7
BusquetsLA <busquetsla@gmail.com>
2022-11-07 12:34:43 -0300

    improving UT for getRequestHeaders

--- httpgo/client_core_test.go
+++ httpgo/client_core_test.go
@@ -15,12 +15,20 @@ func TestGetRequestHeaders(t *testing.T) { // rule of thumb for % of coverage: 1
 
 	// Execution
 	headers := make(http.Header)
-	headers.Set("Accept", "*/*") // 3
-
+	headers.Set("Accept", "*/*")                      // 3
 	finalHeaders := client.getRequestHeaders(headers) // the final list with every added header
 
 	// Validation
 	if len(finalHeaders) != 3 {
-		t.Error("expected 3 headers") // this can be HIGHLY improved
+		t.Error("expected 3 headers") // this can still be HIGHLY improved
+	}
+	if finalHeaders.Get("Content-Type") != "application/json" {
+		t.Error("invalid content type recieved")
+	}
+	if finalHeaders.Get("User-Agent") != "BusquetsLA" {
+		t.Error("invalid user agent recieved")
+	}
+	if finalHeaders.Get("Accept") != "*/*" {
+		t.Error("invalid accept recieved")
 	}
 }
