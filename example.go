a0bf8ae18a46d5c374e19ba5df37a070e29d104a
BusquetsLA <busquetsla@gmail.com>
2022-11-07 11:28:20 -0300

    updated example.go with Post method

--- example.go
+++ example.go
@@ -9,12 +9,32 @@ import (
 )
 
 var (
-	// githubClient = httpgo.New()
 	githubClient = getGithubClient()
 )
 
+type User struct {
+	FirstName string `json:"first_name"` //fields in lowercase will never be exported from the "example" package
+	LastName  string `json:"last_name"`
+}
+
 func main() {
+	lauti := User{"lauti", "busquets"}
+	createUser(lauti)
 	getGithubUrls()
+}
+
+func createUser(user User) {
+	response, err := githubClient.Post("https://api.github.com", nil, user) // ofc returns 404
+	if err != nil {
+		panic(err)
+	}
+
+	bytes, err := ioutil.ReadAll(response.Body)
+	if err != nil {
+		panic(err)
+	}
+
+	fmt.Printf("status code recieved: %v, response body: %v", response.StatusCode, string(bytes))
 }
 
 func getGithubUrls() {
