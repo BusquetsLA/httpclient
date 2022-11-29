cd1c20b11e12852d1e8f3fd7dcd7f4bc44466a1e
BusquetsLA <busquetsla@gmail.com>
2022-11-29 13:37:49 -0300

    example for custom client creation

--- examples/singleton_client.go
+++ examples/singleton_client.go
@@ -12,4 +12,12 @@ func getHttpClient() httpgo.Client {
 	client := httpgo.New(). // here goes all the configurations you would like to add
 				Build() // and finish it with build
 	return client
+
+	// currentClient := http.Client{}
+	// client := httpgo.New().
+	// 	SetResTimeout(3 * time.Second).
+	// 	SetConnTimeout(3 * time.Second).
+	// 	SetHttpClient(&currentClient).
+	// 	Build()
+	// return client
 }
