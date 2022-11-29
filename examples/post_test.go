b72e9864fccbc31b6e496ea780f36529bf4e8bfc
BusquetsLA <busquetsla@gmail.com>
2022-11-29 13:41:29 -0300

    updated to work with new header handling

--- examples/post_test.go
+++ examples/post_test.go
@@ -13,7 +13,7 @@ func TestPost(t *testing.T) {
 	repo := Repository{
 		Name: "testing-repo",
 	}
-	response, err := httpClient.Post("https://api.github.com", nil, repo)
+	response, err := httpClient.Post("https://api.github.com", repo, nil)
 
 	fmt.Println(err)
 	fmt.Println(response)
