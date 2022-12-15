9c0b37fefc8bc5d7dacaa0dfef9c17fb79d2a886
BusquetsLA <busquetsla@gmail.com>
2022-12-15 18:18:24 -0300

    updated post_test, currently not working, need to fix mock_server

--- examples/post_test.go
+++ examples/post_test.go
@@ -2,7 +2,6 @@ package examples
 
 import (
 	"errors"
-	"fmt"
 	"net/http"
 	"testing"
 
@@ -10,33 +9,53 @@ import (
 )
 
 func TestCreateRepository(t *testing.T) {
-	httpgo.ClearMockServer()
-	httpgo.AddMock(httpgo.Mock{
-		Method:  http.MethodPost,
-		Url:     "https://api.github.com/user/repos",
-		ReqBody: `{"name":"testing-repository","description":"","private":true}`,
-		Error:   errors.New("timeout from github"),
+	t.Run("TestErrorPostingInGithub", func(t *testing.T) {
+		errorText := "this is a mock and we need to test when we get an error from github"
+		httpgo.ClearMockServer()
+		httpgo.AddMock(httpgo.Mock{
+			Method:  http.MethodPost,
+			Url:     "https://api.github.com/user/repos",
+			ReqBody: `{"name":"testing-repository","description":"","private":true}`,
+			Error:   errors.New(errorText),
+		})
+		repository := Repository{
+			Name:    "testing-repository",
+			Private: true,
+		}
+		newRepository, err := CreateRepository(repository)
+		if newRepository != nil {
+			t.Error("no new repository expected when we get an error from github")
+		}
+		if err == nil {
+			t.Errorf("error expected: %s", err.Error())
+		}
+		if err.Error() != errorText {
+			t.Errorf(`invalid error message recieved, wanted"%s" but got "%s"`, errorText, err.Error())
+		}
 	})
 
-	repository := Repository{
-		Name:    "testing-repository",
-		Private: true,
-	}
-
-	newRepository, err := CreateRepository(repository)
-
-	if newRepository != nil {
-		t.Error("no new repository expecten when we get a timeout from github")
-	}
-
-	if err == nil {
-		t.Error("error expected when we get a timeout from github")
-	}
-
-	if err.Error() != "timeout from github" {
-		t.Error("invalid error message")
-	}
-
-	fmt.Println(err)
-	fmt.Println(newRepository)
+	t.Run("TestNoError", func(t *testing.T) {
+		httpgo.ClearMockServer()
+		httpgo.AddMock(httpgo.Mock{
+			Method:        http.MethodPost,
+			Url:           "https://api.github.com/user/repos",
+			ReqBody:       `{"name":"testing-repository","private":true}`,
+			ResBody:       `{"id":123,"name":"testing-repository"}`,
+			ResStatusCode: http.StatusCreated,
+		})
+		repository := Repository{
+			Name:    "testing-repository",
+			Private: true,
+		}
+		newRepository, err := CreateRepository(repository)
+		if err != nil {
+			t.Errorf("no error expected, but got: %s", err.Error())
+		}
+		if newRepository == nil {
+			t.Error("new repository expected, but got: nil")
+		}
+		if newRepository.Name != repository.Name {
+			t.Errorf(`expected "%s" as new repository name, but github returned "%s"`, repository.Name, newRepository.Name)
+		}
+	})
 }
