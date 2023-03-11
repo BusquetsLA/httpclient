2f056cba7b0212fbd4549816a13545b00af466ac
BusquetsLA <busquetsla@gmail.com>
2023-03-11 16:43:40 -0300

    added documentation for core package

--- core/client.go
+++ core/client.go
@@ -2,6 +2,8 @@ package core
 
 import "net/http"
 
+// HttpClient is the interface for the http client implementation used by the httpgo package.
+// Any implementation of this interface can be used to send http requests.
 type HttpClient interface {
 	Do(request *http.Request) (*http.Response, error)
 }
--- core/response.go
+++ core/response.go
@@ -6,21 +6,23 @@ import (
 )
 
 type Response struct {
-	Status     string
-	StatusCode int
-	Headers    http.Header
-	Body       []byte
+	Status     string      // HTTP response status
+	StatusCode int         // HTTP response status code
+	Headers    http.Header // HTTP response headers
+	Body       []byte      // HTTP response body
 }
 
-// different types of body return:
+// Bytes returns the HTTP response body as a byte array
 func (r *Response) Bytes() []byte {
 	return r.Body
 }
 
+// String returns the HTTP response body as a string
 func (r *Response) String() string {
 	return string(r.Body)
 }
 
+// UnmarshalJson unmarshals the JSON-encoded HTTP response body into a Go data structure
 func (r *Response) UnmarshalJson(data interface{}) error {
 	return json.Unmarshal(r.Bytes(), data)
 }
